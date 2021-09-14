0c6dfb333455a71ba6165fbf6b7de006c706dd51
Ericka Koyama <ericka.koyama@gmail.com>
2021-09-14 12:45:06 -0700

    [Assignment-2] Coding and testing RESTful controller (#1)
    * Module-2: Creating restful controller methods to add/edit/delete assignments
    
    * remove print debug

--- src/main/java/com/cst438/domain/AssignmentListDTO.java
+++ src/main/java/com/cst438/domain/AssignmentListDTO.java
@@ -13,6 +13,8 @@ public class AssignmentListDTO {
 		public String dueDate;
 		public String courseTitle;
 		public int courseId;
+		
+		public AssignmentDTO() { }
 
 		public AssignmentDTO(int assignmentId, int courseId, String assignmentName, String dueDate,
 				String courseTitle) {
